1bc382ed0013d96c54b0befeba5f1a421abe0446
isabellaOspino <isabellaospinoramirez@gmail.com>
2024-10-08 23:43:58 -0500

    clase modificada en classParqueadero.php

--- classParqueadero.php
+++ classParqueadero.php
@@ -1,55 +1,17 @@
 <?php
-require_once ("classVehiculo");
-class Parqueadero extends Vehiculo {
+class Parqueadero {
+    protected $parqueos = [];
 
-    private $strPuestosDisponibles;
-    private $strPiso ;
-    private $strPrecio; 
-    protected $strStatus = "Ocupado";
-
-public function__construct(string $piso ,string $puestoDisponible , $Precio){
-    $this->strPrecio = $Precio;
-
-    $this->strPuestosDisponibles = array();
-    for($piso = 1; $piso <= $pisos; $piso++){
-        for ($puesto = 1; $puesto <= $puestoDisponible; $puesto++) {
-            $this->epuesto[$piso][$puesto] = $vehiculo; 
-}
-
-}
-
-  }
-  
-  public  function setIngresoVehiculo(){
-    if ($this->puesto[$piso][$puesto]=$vehiculo){
-        echo "ingreso del vehiculo en el piso : $piso, Puesto: $puesto.<br>";
-        
-    }else{
-        echo "puesto ocupado.<br>"
-    }      
-
-}
-
-public function RegistroSalida($piso,$puesto,$horaSalida){
-    $vehiculo= $this->puesto[$piso][$puesto];
-    if $vehiculo-> RegitroSalida($horaSalida)
-    $valorAPagar = $vehiculo->calcularValorPagar($this -> costoPorHora);
-    echo "vehiculo ($vehiculo -> placa) sale . Vlor a pagar : $($valorAPagar )";
-    $this ->puesto[$piso][$puesto];
-}else{
-    echo "el puesto se encuenta desocupado";
-}
-    public function buscarVehiculo($placa){
-        foreach ($this->puesto as $piso => $puestoDisponiblePorPiso){
-            foreach($puestoDisponiblePorPiso as $puesto => $vehiculo){
-                if($vehiculo -> placa === $placa){
-                    echo "el  vehiculo se encuenta en el piso : $piso, Puesto: $puesto.<br>";
-                    echo "informacion : " . $vehiculo->obtenerInformacion()."<br>";
-                    return;
-                }
-            }
-    
-        }
+    public function agregarParqueo(Parqueo $parqueo) {
+        $this->parqueos[] = $parqueo;
     }
-}
 
+    public function buscarVehiculo($placa) {
+        foreach ($this->parqueos as $parqueo) {
+            if ($parqueo->getPlaca() === $placa) {
+                return $parqueo->getInformacion();
+            }
+        }
+        return "";
+    }
+  }
